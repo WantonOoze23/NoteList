95c4dc1971090695cc76766432d76e9b3c0dc3d4
Daniil <mine16978@gmail.com>
2025-04-15 14:47:14 +0300

    migration

--- app/src/main/java/com/tyshko/notelist/MainActivity.kt
+++ app/src/main/java/com/tyshko/notelist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModelProvider
@@ -11,11 +12,25 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.tyshko.notelist.dao.NoteDatabase
 import com.tyshko.notelist.ui.screens.MainScreen
 import com.tyshko.notelist.ui.theme.NoteListTheme
 import com.tyshko.notelist.view.NoteListViewModel
 
 class MainActivity : ComponentActivity() {
+    private val db by lazy{
+        Room.databaseBuilder(
+            applicationContext,
+            NoteDatabase::class.java,
+            name = "notes.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<NoteListViewModel> (
+        //use dagger
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/tyshko/notelist/database/DataBase.kt
+++
@@ -1,5 +0,0 @@
-package com.tyshko.notelist.database
-
-fun dataBase(){
-
-}
